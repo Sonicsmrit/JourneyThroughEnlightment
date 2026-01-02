46623af2ee4c9b13ee374fe00b0f1fc4a3fa83d1
Sonic <sonicsmritshrestha@gmail.com>
2026-01-02 23:36:00 +0545

    ballsandrain

---
+++ RainDrops/Ballsim.py
@@ -0,0 +1,103 @@
+import pygame
+import random
+import math
+import time
+
+pygame.init()
+width, height = (800, 800)
+screen = pygame.display.set_mode((width,height))
+running = True
+
+#v = u + at; f= ma; a = f/m; m=10, ke = 1/2 mv^2 
+
+
+class Balls:
+    def __init__(self):
+        self.y = 0
+        self.x = random.randint(0, width)
+        self.distance = None
+        self.vy = 0
+        self.colour = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.size = random.uniform(10,18)
+        self.vx = 0
+        self.last_time = time.time()
+        
+
+    def update(self):
+        start_time = time.time()
+        gravity = 100
+        
+        
+        dt = start_time - self.last_time
+        
+        self.vy += gravity*dt *self.size #a = gt
+
+        self.last_time = start_time
+        self.y += self.vy * dt #v = u + at; a = gt
+
+        
+        self.x += self.vx * dt
+        
+
+
+        if self.y >= 800-self.size:
+
+            # KE = 0.5 * self.mass * self.speed ** 2
+            # acceleration = gravity
+            
+            # force = acceleration*self.mass
+
+            # self.y -= KE/force
+            # THis formula just calc the distance dute to which  it teleported to that distance, thats why i introduced velocity
+
+            
+
+            self.y = height - self.size
+            bounce = (self.size/20) + 0.1
+            bounce = max(0.3,bounce)
+            self.vy *= -bounce
+            self.vx *= -0.5
+            self.vx += random.uniform(-100, 100) 
+        
+        if self.x >= 800-self.size:
+            self.x = width - self.size
+            self.vx -= 500
+        if self.x <= 0 + self.size:
+            self.x = 0 + self.size
+            self.vx += 500
+
+
+
+
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.colour, (self.x, self.y), self.size)
+            
+
+
+
+
+
+
+balls = []
+for i in range(100):
+    balls.append(Balls())
+
+
+
+while running:
+
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            running = False
+
+    
+    screen.fill('white')
+    for ball in balls:
+        ball.update()
+        ball.draw(screen)
+
+    
+    pygame.display.flip()
+
+pygame.quit()
---
+++ RainDrops/Raindrop.py
@@ -0,0 +1,67 @@
+import random
+import time
+import pygame
+
+
+pygame.init()
+screen = pygame.display.set_mode((800,800))
+
+class Drops:
+
+    def __init__(self):
+        self.x = random.randint(0,800)
+        self.y = 0
+        self.speed = random.randint(200,1000)
+        self.color = (random.randint(0,255),random.randint(0,255), random.randint(0,255))
+        self.last_time = time.time()
+    
+    def update(self):
+        current_time = time.time()
+        dt = current_time - self.last_time
+        self.last_time = current_time
+
+        self.y += self.speed * dt  #falling of balls
+
+        #position
+
+        #color
+        
+
+        # print(self.color)
+
+        if self.y > 800:
+            self.y = 0
+            self.x = random.randint(0,800)
+
+
+        # print(self.y)
+    
+    def circle(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x,self.y), 5)
+
+
+# while True:
+#     fall.update()
+#     time.sleep(1)
+
+
+rain = []
+for x in range(100):
+    rain.append(Drops())
+    
+
+running = True
+
+while running:
+
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            running = False
+
+    
+    screen.fill('white')
+    for drop in rain:
+        drop.update()
+        drop.circle(screen)
+    
+    pygame.display.flip()
